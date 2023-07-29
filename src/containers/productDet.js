ec028b0de71cb71c348cc2903c3d79fe720207bb
giomax <kvavilashvili@gmail.com>
2023-07-29 21:22:49 +0400

    curator

--- src/containers/productDet.js
+++ src/containers/productDet.js
@@ -259,7 +259,7 @@ export default function ProductDet(props) {
               </div>
               <div className="size">
                 <p>ID: {artwork.id}</p>
-                <p className="curat">{artwork.curator_id && <>Curated by <a href="${`/curator/artwork.curator_id`}" className="underline">Ria Keburia</a></>}</p>
+                <p className="curat">{artwork.curator_id && <>Curated by <a href={`/curator/${artwork.curator_id}`} className="underline">{artwork?.display_name}</a></>}</p>
                 <p className='orig'>Certificate of Authenticity</p>
               </div>
 
