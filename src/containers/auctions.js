8526bcef55bf68133f99404dea7b92876f446c74
giomax <kvavilashvili@gmail.com>
2024-03-26 15:21:38 +0400

    changed arrow

--- src/containers/auctions.js
+++ src/containers/auctions.js
@@ -11,6 +11,7 @@ import Loading from "./loading";
 import { MetaTags } from "react-meta-tags";
 const queryClient = new QueryClient();
 
+
 function Auctions(props) {
   const [filter, setFilter] = React.useState(false);
   const { isLoading, error, data } = useQuery("auctions", getAuctions, {
@@ -55,7 +56,12 @@ function Auctions(props) {
                     <span className="date">{item.city} {item.date_formatted}</span>
                   </div>
                   <div className="col-md-3">
-                    <span className="arrow">→</span>
+                    <span className="arrow">
+                    <svg width="24" height="8" viewBox="0 0 24 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17.728 7.8C18.048 7.11733 18.3573 6.52 18.656 6.008C18.976 5.496 19.2853 5.06933 19.584 4.728H0.928V3.384H19.584C19.2853 3.02133 18.976 2.584 18.656 2.072C18.3573 1.56 18.048 0.973332 17.728 0.311999H18.848C20.192 1.86933 21.6 3.02133 23.072 3.768V4.344C21.6 5.06933 20.192 6.22133 18.848 7.8H17.728Z" fill="black"/>
+</svg>
+
+                    </span>
                   </div>
                 </div>
                 </Link></div>
@@ -75,7 +81,13 @@ function Auctions(props) {
                     <span className="date">{item.city} {item.date_formatted}</span>
                   </div>
                   <div className="col-md-3">
-                    <span className="arrow">→</span>
+                    <span className="arrow">
+                      
+                    <svg width="24" height="8" viewBox="0 0 24 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17.728 7.8C18.048 7.11733 18.3573 6.52 18.656 6.008C18.976 5.496 19.2853 5.06933 19.584 4.728H0.928V3.384H19.584C19.2853 3.02133 18.976 2.584 18.656 2.072C18.3573 1.56 18.048 0.973332 17.728 0.311999H18.848C20.192 1.86933 21.6 3.02133 23.072 3.768V4.344C21.6 5.06933 20.192 6.22133 18.848 7.8H17.728Z" fill="black"/>
+</svg>
+
+                    </span>
                   </div>
                 </div>
                 </Link></div>
